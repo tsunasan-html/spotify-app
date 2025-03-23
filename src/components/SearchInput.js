a345fb464da663cc62e13920397cd055afed8879
tsunasan-html <>
2025-03-23 14:48:01 +0900

    add css

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,16 +1,17 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "../assets/style.css"
 
 export function SearchInput(props) {
   return (
     <section className="mb-10">
       <input
         onChange={props.onInputChange}
-        className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none"
+        className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none searchInput"
         placeholder="探したい曲を入力してください"
       />
       <button onClick={props.onSubmit}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg">
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg searchButton">
         <FontAwesomeIcon icon={faSearch} />
       </button>
     </section>
